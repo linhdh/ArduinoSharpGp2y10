4a8ef9edd6d18afa8875f0eab45819ba3f716572
linhdh <linhdh@live.com>
2022-05-22 16:37:26 +0700

    add Microchip studio solution

---
+++ MicrochipStudio/SharpGP2Y10/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 5/22/2022 4:35:42 PM
+ * Author : PhuongNT
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ MicrochipStudio/SharpGP2Y10/SharpGp2y10/Sketch.cpp
@@ -0,0 +1,31 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+#include <GP2YDustSensor.h>
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+
+
+int measurePin = A0;
+int ledPin = 2;
+
+GP2YDustSensor dustSensor(GP2YDustSensorType::GP2Y1014AU0F, ledPin, measurePin);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dustSensor.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Dust density: ");
+  Serial.print(dustSensor.getDustDensity());
+  Serial.print(" ug/m3; Running average: ");
+  Serial.print(dustSensor.getRunningAverage());
+  Serial.println(" ug/m3");  
+  delay(1000);
+}
